c5e42fccb22249fe1b76f5ec52ac651a638c4aca
shashiranjan <sssepn@gmail.com>
2019-10-19 16:52:45 +0530

    changes in partner

--- lookspace_app/urls.py
+++ lookspace_app/urls.py
@@ -2,5 +2,23 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = "home"),
+
+]
+
+
+urlpatterns = [
+    path('index', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('', views.home_page, name = "home_page"),
+
+    path('customers/', include(([
+        path('', views.index, name='quiz_list'),
+    ], 'spaceapp'), namespace='customers')),
+
+    path('partners/', include(([
+        path('', views.space_details_subsriptions, name='quiz_change_list'),
+        path('details', views.all_space_details, name='space_details'),
+         path('edit/<int:id>', views.edit_space_details, name='edit_details'),
+    ], 'spaceapp'), namespace='partners')),
+
 ]
