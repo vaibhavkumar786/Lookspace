367d9c85e6359aa4ca875897fff38bebaf6ff191
Vaibhav Kumar <vaibhav.kumar@mountblue.io>
2019-10-19 16:00:21 +0530

    Add signin and signup urls

--- lookspace_app/urls.py
+++ lookspace_app/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('', views.home, name = "home"),
-]
+    path('', views.index, name = "index"),
+    path('user_signup/', views.user_signup, name = "user_signup"),
+    path('user_signin/', views.user_signin, name = "user_signin"),
+    path('partner_signup/', views.partner_signup, name = "partner_signup"),
+    path('partner_signin/', views.partner_signin, name = "partner_signin"),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
