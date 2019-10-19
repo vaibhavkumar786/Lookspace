39181f70e160eafa1f834d8a956118e95ede3f5a
Vaibhav Kumar <vaibhav.kumar@mountblue.io>
2019-10-19 15:50:28 +0530

    Database change

--- lookspace/settings.py
+++ lookspace/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'lookspace.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD':os.getenv('DB_PASSWORD'),
@@ -126,3 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+
