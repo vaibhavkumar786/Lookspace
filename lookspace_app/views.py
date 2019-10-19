ac2a255a6138327555ad0c456ca3c0fcffcfeb2f
Vaibhav Kumar <vaibhav.kumar@mountblue.io>
2019-10-19 16:01:37 +0530

    Add routes: signin, signup

--- lookspace_app/views.py
+++ lookspace_app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return render(request, 'lookspace_app/home.html')
+def index(request):
+    return render(request, 'lookspace_app/html/index.html')
+
+def user_signup(request):
+    return render(request, 'lookspace_app/html/user_signup.html')
+
+def user_signin(request):
+    return render(request, 'lookspace_app/html/user_signin.html')
+
+def partner_signup(request):
+    return render(request, 'lookspace_app/html/partner_signup.html')
+
+def partner_signin(request):
+    return render(request, 'lookspace_app/html/partner_signin.html')
+
 
 
