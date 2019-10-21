b55b97fef4bf542b25edc399598edfea066f991e
Vaibhav Kumar <vaibhav.kumar@mountblue.io>
2019-10-22 00:10:38 +0530

    Fix some changes

--- lookspace_app/admin.py
+++ lookspace_app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import SpaceDetails , User, BookedSeats, ScheduleVisit
+from .models import SpaceDetails , User, BookedSeats, ScheduleVisit, ContactForm
 
 admin.site.register(User)
 admin.site.register(SpaceDetails)
 admin.site.register(BookedSeats)
 admin.site.register(ScheduleVisit)
+admin.site.register(ContactForm)
